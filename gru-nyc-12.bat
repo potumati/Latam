dae36eff856187a07daa5212b8d1bb97ae3a43a0
potumati <32221653+potumati@users.noreply.github.com>
2025-04-05 21:01:47 -0400

    Update .gitignore to exclude .bat files and remove unused batch scripts

--- gru-nyc-12.bat
+++
@@ -1,6 +0,0 @@
-python latam3.py --origin=GRU --destination=NYC --data_inicial=2025-12-01 --quantidade_dias_tentar=10 --tempo_de_viagem=6 --dias_a_mais_pode_durar=2
-
-python latam3.py --origin=GRU --destination=NYC --data_inicial=2025-12-11 --quantidade_dias_tentar=10 --tempo_de_viagem=6 --dias_a_mais_pode_durar=2
-
-python latam3.py --origin=GRU --destination=NYC --data_inicial=2025-12-21 --quantidade_dias_tentar=10 --tempo_de_viagem=6 --dias_a_mais_pode_durar=2
-
